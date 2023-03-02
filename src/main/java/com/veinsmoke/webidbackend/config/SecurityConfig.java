016dee012f246e3e9ab3d320e216391dcc5e03b6
TahaLechgar <tahamr08@gmail.com>
2023-03-02 16:23:53 +0100

    configure AuthenticationManager, AuthenticationProvider, PasswordEncoder and UserDetailsService

---
+++ src/main/java/com/veinsmoke/webidbackend/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.veinsmoke.webidbackend.config;
+
+import com.veinsmoke.webidbackend.security.JWTAuthFilter;
+import com.veinsmoke.webidbackend.service.UserDetailsServiceImp;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsServiceImp userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authManager(AuthenticationProvider authenticationProvider)  {
+        return new ProviderManager(List.of(authenticationProvider));
+    }
+
+}
